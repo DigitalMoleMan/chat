47274e49b3716cc81ea5576ca302617c266f0fb7
Olle <yogsther@gmail.com>
2018-08-30 10:43:31 +0200

    Chat improvementes, image support and ping sound

--- code.js
+++ code.js
@@ -7,6 +7,9 @@ socket.on("post", (data) => {
     load()
 })
 
+var ping = new Audio();
+    ping.src = "sounds/ping.mp3";
+
 function load() {
 
     function compare(a, b) {
@@ -19,11 +22,23 @@ function load() {
 
     posts.sort(compare);
 
+    posts.forEach(post => {
+        post.message = post.message.split(" ");
+        post.message.forEach((msg, i) => {
+            if(msg.substr(msg.length-4, msg.length-1) == ".png") post.message[i] = "<img class='image-attachment' src='" + post.message[i] + "'>";
+
+        })
+        post.message = post.message.join(" ");
+    })
+
     var cards = "";
     posts.forEach(post => {
         cards += '<div id="card" class="card"> <span id="messagePoster" class="posterName">' + post.name + '</span> <span id="messageContent" class="messageContent">' + post.message + '</span> </div>';
     })
     document.getElementById("contentArea").innerHTML = cards;
+
+    var objDiv = document.getElementById("contentArea");
+    objDiv.scrollTop = objDiv.scrollHeight;
 
 }
 
@@ -33,16 +48,43 @@ document.addEventListener('keydown', function (event) {
     }
 })
 
+socket.on("message", message => {
+
+    message.message = message.message.split(" ");
+    message.message.forEach(msg => {
+        if(msg.substr(msg.length-4, msg.length-1) == ".png") message.message[i] = "<img class='image-attachment' src='" + message.message[i] + "'>";
+    })  
+    message.message = message.message.join(" ");
+
+    document.getElementById("contentArea").innerHTML += '<div id="card" class="card"> <span id="messagePoster" class="posterName">' + message.name + '</span> <span id="messageContent" class="messageContent">' + message.message + '</span> </div>';
+
+    var objDiv = document.getElementById("contentArea");
+    objDiv.scrollTop = objDiv.scrollHeight;
+
+    ping.play();
+
+
+})
+
 function submit(message, name) {
     if (!message == "") {
+
+        var err = "";
+        if(message.trim() == "" || name.trim() == "") err = "Ops, no message or name."
+        if(err != "") {
+            alert(err);
+            return;
+        }
+
+
         socket.emit("submit", {
             name: name,
             message: message
         });
 
+
         document.getElementById("messageInput").value = "";
 
-
-        document.getElementById("contentArea").innerHTML = '<div id="card" class="card"> <span id="messagePoster" class="posterName">' + name + '</span> <span id="messageContent" class="messageContent">' + message + '</span> </div>' + document.getElementById("contentArea").innerHTML;
+        //document.getElementById("contentArea").innerHTML = '<div id="card" class="card"> <span id="messagePoster" class="posterName">' + name + '</span> <span id="messageContent" class="messageContent">' + message + '</span> </div>' + document.getElementById("contentArea").innerHTML;
     }
 }
